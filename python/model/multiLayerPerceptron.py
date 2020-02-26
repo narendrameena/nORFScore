2f6dabc7dd4c4f5b00b0d8874483abe844558c98
narendrameena <narendrabhagri03@gmail.com>
2020-02-26 15:23:59 +0000

    firs successful MLP model

--- python/model/multiLayerPerceptron.py
+++ python/model/multiLayerPerceptron.py
@@ -1,119 +1,91 @@
 #!/usr/bin/env python3 
-###-----------------------------------------------------------------------------##
-#
-#@author narumeena
-#@descripton class and function to implement multilayer perceptron 
-#@inspiration https://medium.com/biaslyai/pytorch-introduction-to-neural-network-feedforward-neural-network-model-e7231cff47cb
-#
-###-----------------------------------------------------------------------------##
+#example 
 
-import torch 
-import torch.nn as nn
-import torch.nn.functional as F 
-import numpy
-
-
-
-"""
-class multiLayerPerceptron(nn.Module):
-    def __init__(self):
-        super(multiLayerPerceptron, self).__inti__()
-        #input layer 
-        self.inputLayer             =   nn.Linear(76,40)
-        self.relu                   =   nn.ReLU()
-        #first hidden layer 
-        self.hiddenLayer            =   nn.Linear(40,15)
-        self.relu                   =   nn.ReLU()
-        #output layer 
-        self.outputLayer            =   nn.Linear(15,2)
-        self.sigmoid                =   nn.Sigmoid()
-
-
-    def forward(self,x):
-        x   =   F.relu(self.inputLayer(x))
-        x   =   F.relu(self.hiddenLayer(x))
-        x   =   self.outputLayer(x)
-        return x
-
-"""
+import torch
+import numpy 
+import pandas as pd
+from sklearn.model_selection import train_test_split
 
 
 
 class Feedforward(torch.nn.Module):
-        def __init__(self, input_size, hidden_size):
-            super(Feedforward, self).__init__()
-            self.input_size = input_size
-            self.hidden_size  = hidden_size
-            self.fc1 = torch.nn.Linear(self.input_size, self.hidden_size)
-            self.relu = torch.nn.ReLU()
-            self.fc2 = torch.nn.Linear(self.hidden_size, 1)
-            self.sigmoid = torch.nn.Sigmoid()
-        def forward(self, x):
-            hidden = self.fc1(x)
-            relu = self.relu(hidden)
-            output = self.fc2(relu)
-            output = self.sigmoid(output)
-            return output
-
+    def __init__(self, input_size, hidden_size):
+        super(Feedforward, self).__init__()
+        self.input_size = input_size
+        self.hidden_size  = hidden_size
+        self.fc1 = torch.nn.Linear(self.input_size, self.hidden_size)
+        self.relu = torch.nn.ReLU()
+        self.fc2 = torch.nn.Linear(self.hidden_size, 1)
+        self.sigmoid = torch.nn.Sigmoid()
+    def forward(self, x):
+        hidden = self.fc1(x)
+        relu = self.relu(hidden)
+        output = self.fc2(relu)
+        output = self.sigmoid(output)
+        return output
 
 
 
 def main():
+
+
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     
     print(device)
-    device  =   'cpu'
-    print(device)
-    # CREATE RANDOM DATA POINTS
-    from sklearn.datasets import make_blobs
-    def blob_label(y, label, loc): # assign labels
-        target = numpy.copy(y)
-        for l in loc:
-            target[y == l] = label
-        return target
-    x_train, y_train = make_blobs(n_samples=400000, n_features=76, cluster_std=1.5, shuffle=True)
-    print(x_train[0:5,:])
-    print(y_train[0:5])
-    x_train = torch.FloatTensor(x_train,device=device)
-    y_train = torch.FloatTensor(blob_label(y_train, 0, [0]),device=device)
-    y_train = torch.FloatTensor(blob_label(y_train, 1, [1,2,3]),device=device)
-    x_test, y_test = make_blobs(n_samples=10, n_features=76, cluster_std=1.5, shuffle=True)
-    x_test = torch.FloatTensor(x_test,device=device)
-    y_test = torch.FloatTensor(blob_label(y_test, 0, [0]),device=device)
-    y_test = torch.FloatTensor(blob_label(y_test, 1, [1,2,3]),device=device)
 
+    data    =   pd.read_csv("../../../analysis/trainingDataSet/training_one_hot_encoding_Coding_region_only.csv", sep="\t")   
 
-    model = Feedforward(76, 10)
+    labels  =   data['category']
+    # Split the data into training and testing sets
+    X_train, X_test, Y_train, Y_test    =   train_test_split(data.drop('category', axis = 1), labels, test_size = 0.25, random_state = 100)
+
+    x_train =   torch.cuda.DoubleTensor(X_train.values).float()
+    print(x_train.shape)
+    y_train =   torch.cuda.DoubleTensor(Y_train.values).float()
+    x_test  =   torch.cuda.DoubleTensor(X_test.values).float()
+    print(x_test.shape)
+    y_test  =   torch.cuda.DoubleTensor(Y_test.values).float()
+
+    model = Feedforward(73, 10).to(device)
     criterion = torch.nn.BCELoss()
     optimizer = torch.optim.SGD(model.parameters(), lr = 0.01)
 
-    model.eval()
-    y_pred = model(x_test)
+
+    model.eval().to(device)
+    y_pred = model(x_test).to(device)
     before_train = criterion(y_pred.squeeze(), y_test)
     print('Test loss before training' , before_train.item())
 
 
-    model.train()
-    epoch = 10
+
+    model.train().to(device)
+    epoch = 100000000
     for epoch in range(epoch):
         optimizer.zero_grad()
         # Forward pass
-        y_pred = model(x_train)
+        y_pred = model(x_train).to(device)
         # Compute Loss
         loss = criterion(y_pred.squeeze(), y_train)
    
         print('Epoch {}: train loss: {}'.format(epoch, loss.item()))
-        # Backward pass
-        loss.backward()
-        optimizer.step()
+
 
         #Additional Info when using cuda
-        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-        if device == 'cuda:0':
+        if device.type == 'cuda':
             print(torch.cuda.get_device_name(0))
             print('Memory Usage:')
             print('Allocated:', round(torch.cuda.memory_allocated(0)/1024**3,1), 'GB')
             print('Cached:   ', round(torch.cuda.memory_cached(0)/1024**3,1), 'GB')
+
+        # Backward pass
+        loss.backward()
+        optimizer.step()
+
+    model.eval()
+    y_pred = model(x_test).to(device)
+    after_train = criterion(y_pred.squeeze(), y_test) 
+    print('Test loss after Training' , after_train.item())
+
 
 
 if __name__ == "__main__":
